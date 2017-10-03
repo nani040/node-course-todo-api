d301fa9fba94493c9f6eb8535f93c0812809512b
naveen <nani040@gmail.com>
2017-10-03 10:26:20 +0530

    add DELETE/ todo/:id

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ var {User} = require('./models/user');
 
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -47,8 +49,24 @@ app.get('/todos/:id',(req, res) => {
 
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.delete('/todos/:id',(req, res) => {
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send()
+	}
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo){
+			return res.status(404).send()
+		}
+		res.send(todo);
+	}).catch((e) => {
+		res.status(400).send();
+	});
+
+});
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
